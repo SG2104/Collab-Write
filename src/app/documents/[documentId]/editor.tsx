081ec358e3eabee4545bce971b588aa6ba1acb7b
SG2104 <shreyalganna21@gmail.com>
2025-02-20 20:56:43 -0500

    setup the margin ruler

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -25,8 +25,13 @@ import { LineHeightExtension } from "@/extensions/line-height";
 import { useLiveblocksExtension } from "@liveblocks/react-tiptap";
 import { Threads } from "./Threads";
 
+import { useStorage } from "@liveblocks/react/suspense";
+
 export const Editor = () => {
  
+  const leftMargin = useStorage((root) => root.leftMargin);
+  const rightMargin = useStorage((root) => root.rightMargin);
+
   const liveblocks = useLiveblocksExtension();
   const { setEditor } = useEditorStore();
 
@@ -60,7 +65,7 @@ export const Editor = () => {
     },
     editorProps: {
       attributes: {
-        style: "padding-left: 56px; padding-right: 56px;",
+        style: `padding-left: ${leftMargin ?? 56}px; padding-right: ${rightMargin ?? 56}px;`,
         class:
           "focus:outline-none print:border-0 bg-white border border-[#C7C7CC] min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
